f67554bcdb0900dd155bcd7033c06b342d823c36
danicingsun <tsveta.ss@googlemail.com>
2026-01-03 22:50:23 +0100

    more fixes

--- actions.py
+++ actions.py
@@ -135,6 +135,7 @@ class ValidateBookingForm(FormValidationAction):
         if norm in {"refundable", "non-refundable", "nonrefundable"}:
             dispatcher.utter_message(f"Refund policy set to {norm}.")
             return {"refund": norm}
+        
         dispatcher.utter_message("Please choose refundable or non-refundable.")
         return {"refund": None}
 
